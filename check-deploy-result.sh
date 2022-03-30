3665608e34557a85701b4012bc16b58448f4c6db
angusyang@apjcorp.com <angusyang@apjcorp.com>
2022-03-30 09:34:17 +0800

    update

--- check-deploy-result.sh
+++ check-deploy-result.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-echo $PWD
+echo "Path="$PWD
 echo "Hello"
+DEPLOY_RESULT=""
+if [ -f "deploy-result-app31.txt" ]; then
+  echo "in app31"
+  if [ `grep -c "Fail:" deploy-result-app31.txt` -ne '0' ]; then
+    DEPLOY_RESULT="App31 deploy failed;"
+  fi
+  rm deploy-result-app31.txt
+fi
+if [ -f "deploy-result-app32.txt" ]; then
+  echo "in app32"
+  if [ `grep -c "Fail:" deploy-result-app32.txt` -ne '0' ]; then
+    DEPLOY_RESULT=$DEPLOY_RESULT"App32 deploy failed;"
+  fi
+  rm deploy-result-app32.txt
+fi
+if [ -n "${DEPLOY_RESULT}" ]; then
+  echo "::error::${DEPLOY_RESULT}"
+  exit 1
+fi
