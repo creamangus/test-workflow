51259fa94e42db62af75dc7c9275472475716419
angusyang@apjcorp.com <angusyang@apjcorp.com>
2022-03-30 09:18:18 +0800

    test

--- deployment/tt/check-deploy-result.sh
+++ deployment/tt/check-deploy-result.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+echo $PWD
 DEPLOY_RESULT=""
 if [ -f "../../deploy-result-app31.txt" ]; then
+  echo "in app31"
   if [ `grep -c "Fail:" ../../deploy-result-app31.txt` -ne '0' ]; then
     DEPLOY_RESULT="App31 deploy failed;"
   fi
   rm ../../deploy-result-app31.txt
 fi
 if [ -f "../../deploy-result-app32.txt" ]; then
+  echo "in app32"
   if [ `grep -c "Fail:" ../../deploy-result-app32.txt` -ne '0' ]; then
     DEPLOY_RESULT=$DEPLOY_RESULT"App32 deploy failed;"
   fi
